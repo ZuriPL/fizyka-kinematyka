bb03f5bec787d96f77ed2a7edb8dc18569441346
ZuriPL <michal.bak222@gmail.com>
2022-09-13 22:21:03 +0200

    1

--- index.js
+++ index.js
@@ -42,21 +42,11 @@ function move(dt) {
 	y -= v * dt
 }
 
-function max(a, b) {
-	if (+a < +b) return b
-
-	return a
-}
-
 ctx.fillStyle = '#ff1111'
+ctx.lineWidth = 2
 
 function animate(x) {
 	ctx.clearRect(0, 0, canvas.width, canvas.height)
-
-	ctx.beginPath()
-	ctx.arc(canvas.width / 4, canvas.height - y * 100, 16, Math.PI * 2, 0, false)
-	ctx.closePath()
-	ctx.fill()
 
 	ctx.strokeStyle = 'gray'
 
@@ -66,12 +56,20 @@ function animate(x) {
 
 	ctx.strokeStyle = '#000000'
 
+	// eww
 	ctx.beginPath()
 	ctx.moveTo(canvas.width / 4 - 75, canvas.height - heightInput.value)
-	ctx.lineTo(canvas.width / 4 + 1, max(canvas.height - heightInput.value, canvas.height - y * 100 + 16))
-	ctx.moveTo(canvas.width / 4, max(canvas.height - heightInput.value, canvas.height - y * 100 + 16))
+	ctx.lineTo(canvas.width / 4 - 9, Math.max(canvas.height - heightInput.value, canvas.height - y * 100 + 16))
+	ctx.moveTo(canvas.width / 4 - 10, Math.max(canvas.height - heightInput.value, canvas.height - y * 100 + 16))
+	ctx.lineTo(canvas.width / 4 + 11, Math.max(canvas.height - heightInput.value, canvas.height - y * 100 + 16))
+	ctx.moveTo(canvas.width / 4 + 10, Math.max(canvas.height - heightInput.value, canvas.height - y * 100 + 16))
 	ctx.lineTo(canvas.width / 4 + 75, canvas.height - heightInput.value)
 	ctx.stroke()
+
+	ctx.beginPath()
+	ctx.arc(canvas.width / 4, canvas.height - y * 100, 16, Math.PI * 2, 0, false)
+	ctx.closePath()
+	ctx.fill()
 
 	if (isRunning) {
 		move(((x - lastT) / 1000) * timeMultiplierInput.value)
@@ -83,11 +81,12 @@ function animate(x) {
 	lastT = x
 
 	if (y < 0) {
-		isRunning = false
-
 		console.log(`Czas: ${(Date.now() - startTime) / 1000} s`)
 		console.log(`Prędkość: ${v} m/s`)
 		console.log(`Y: ${y}`)
+
+		isRunning = false
+		startButton.textContent = 'Start'
 
 		y = +ballInput.value
 		v = 0
@@ -102,9 +101,11 @@ function animate(x) {
 function toggle() {
 	if (isRunning) {
 		isRunning = false
+		startButton.textContent = 'Start'
 	} else {
 		isRunning = true
 		startTime = Date.now()
+		startButton.textContent = 'Stop'
 	}
 }
 
